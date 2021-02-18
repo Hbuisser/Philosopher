2ee04681de232f27454ec2fad2b4c7f9e5f8de08
Henry Buisseret <henrybuisseret@gmail.com>
2021-02-18 17:23:58 +0100

    time

--- philo_one.c
+++ philo_one.c
@@ -6,7 +6,7 @@
 /*   By: hbuisser <hbuisser@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/25 17:33:37 by hbuisser          #+#    #+#             */
-/*   Updated: 2021/02/18 16:30:01 by hbuisser         ###   ########.fr       */
+/*   Updated: 2021/02/18 17:23:27 by hbuisser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,12 +25,25 @@ int destroy_mutex(t_data *values)
 	return (0);
 }
 
+int thinking(t_data *values, int i)
+{
+	values->nbr_of_time_each_philo_must_eat = 0;
+	printf("Philo %d pense\n", i + 1);
+	return (0);
+}
+
 int sleeping(t_data *values, int i)
 {
-	i = 0;
-	//write(2, "Philo dort\n", 11);
-	printf("Philo %d dort\n", values->philo_num[i]);
+	struct timeval temps_avant;
+	struct timeval temps_apres;
+	long int time;
+
+	printf("Philo %d dort\n", i + 1);
+	gettimeofday (&temps_avant, NULL);
 	usleep(values->time_to_sleep);
+	gettimeofday (&temps_apres, NULL);
+	time = ((((temps_apres.tv_sec - temps_avant.tv_sec) * 1000000 + temps_apres.tv_usec) - temps_avant.tv_usec) * 1000);
+	printf("Philo %d a terminé de dormir apres %ld ms\n", i + 1, time);
 	return (0);
 }
 
@@ -40,7 +53,7 @@ int eating(t_data *values, int i)
 	struct timeval temps_apres;
 	long int time;
 	
-	printf("Philo %d mange\n", i + 1);
+	printf("Philo %d is eating\n", i + 1);
 	gettimeofday (&temps_avant, NULL);
 	usleep(values->time_to_eat);
 	gettimeofday (&temps_apres, NULL);
@@ -52,18 +65,29 @@ int eating(t_data *values, int i)
 void *routine(void *arg)
 {
 	t_data *values;
-	int i;
+	int fork;
+	int next_fork;
+	struct timeval temps_avant;
+	struct timeval temps_apres;
+	long int time;
 
 	values = get_struct();
-	i = *(int *)arg;
-	pthread_mutex_lock(&values->mutex[i]);
-	if (&values->mutex[i + 1])
-		pthread_mutex_lock(&values->mutex[i + 1]);
-	eating(values, i);
-	pthread_mutex_unlock(&values->mutex[i]);
-	if (&values->mutex[i + 1])
-		pthread_mutex_unlock(&values->mutex[i + 1]);
-	sleeping(values, i);
+	fork = *(int *)arg;
+	next_fork = (fork + 1) % values->nbr_of_philo;
+	time = ((((temps_apres.tv_sec - temps_avant.tv_sec) * 1000000 + temps_apres.tv_usec) - temps_avant.tv_usec) * 1000);
+	//gettimeofday (&temps_avant, NULL);
+	while (1)
+	{
+		pthread_mutex_lock(&values->mutex[fork]);
+		pthread_mutex_lock(&values->mutex[next_fork]);
+		//gettimeofday (&temps_apres, NULL);
+		//printf("Philo %d has taken a fork %ld ms\n", fork, time);
+		eating(values, fork);
+		pthread_mutex_unlock(&values->mutex[fork]);
+		pthread_mutex_unlock(&values->mutex[next_fork]);
+		sleeping(values, fork);
+		thinking(values, fork);
+	}
 	return (arg);
 }
 
@@ -79,6 +103,7 @@ int init_and_malloc_mutex_and_philo(t_data *values)
 	while (i < values->nbr_of_philo)
 	{
 		pthread_mutex_init(&values->mutex[i], NULL);
+		//thinking(values, i);
 		i++;
 	}
 	return (0);
